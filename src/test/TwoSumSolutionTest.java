e788968f333038292f659d6a30c848d1b7aad324
Yiting Yao <yao00043@algonquinlive.com>
2024-05-25 14:48:21 -0400

    Update

--- src/test/TwoSumSolutionTest.java
+++ src/test/TwoSumSolutionTest.java
@@ -1,7 +1,6 @@
 import main.TwoSumSolution;
 import org.junit.Test;
 
-
 import static org.junit.Assert.assertArrayEquals;
 
 public class TwoSumSolutionTest {
@@ -9,11 +8,9 @@ public class TwoSumSolutionTest {
     @Test
     public void testTwoSum() {
         TwoSumSolution solution = new TwoSumSolution();
-        // Test where the expected indices are 0 and 1 for the sum to match target
-        assertArrayEquals(new int[]{0,1}, solution.twoSum(new int[]{2, 7, 11, 15}, 9));
-        assertArrayEquals(new int[]{1,2}, solution.twoSum(new int[]{3, 2, 4}, 6));
-        assertArrayEquals(new int[]{0,1}, solution.twoSum(new int[]{3, 3}, 6));
+        assertArrayEquals(new int[]{0, 1}, solution.twoSum(new int[]{2, 7, 11, 15}, 9));
+        assertArrayEquals(new int[]{1, 2}, solution.twoSum(new int[]{3, 2, 4}, 6));
+        assertArrayEquals(new int[]{0, 1}, solution.twoSum(new int[]{3, 3}, 6));
 
-        // Additional tests can be added to cover more scenarios
     }
 }
